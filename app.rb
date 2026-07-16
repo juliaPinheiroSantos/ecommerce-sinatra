fde04f9b231e6f46a38c6db76aa1666140347fc4
Giovannah Costa <giovannahgsantos@gmail.com>
2026-07-16 10:42:23 -0300

    Finaliza telas de autenticacao, perfil e navegacao

---
+++ app.rb
@@ -0,0 +1,42 @@
+require "sinatra"
+require "sinatra/activerecord"
+require "sinatra/flash"
+
+enable :sessions
+
+get "/" do
+    "Iniciando o projeto de E-commerce"
+end
+
+get "/cadastro" do
+  erb :cadastro
+end
+
+post '/cadastro' do
+  
+  flash[:notice] = "Os dados foram enviados com sucesso!"
+  
+  redirect '/'
+end
+
+post '/login' do
+ 
+  session[:usuario_id] = 1 
+  flash[:notice] = "Login realizado com sucesso!"
+  redirect '/'
+end
+
+get '/logout' do
+  session.clear
+  flash[:notice] = "Você saiu da sua conta."
+  redirect '/'
+end
+
+get '/perfil' do
+  if session[:usuario_id]
+    erb :perfil
+  else
+    flash[:error] = "Você precisa fazer login primeiro."
+    redirect '/login'
+  end
+end
